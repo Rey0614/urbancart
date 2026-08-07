8facdb361c01dbafc9da97bbbb827562dd6d2ba4
Rey <95080700+Rey0614@users.noreply.github.com>
2026-08-07 17:27:15 +0700

    Finished Task 22

---
+++ queries/challenge.sql
@@ -0,0 +1,16 @@
+--Task 20
+SELECT *
+FROM Products
+WHERE unit_price BETWEEN 50 AND 200
+
+--Task 21
+SELECT *
+FROM Customers
+WHERE city IN ('Jakarta', 'Surabaya', 'Bandung')
+
+--Task 22
+SELECT *
+FROM Products
+WHERE product_name LIKE '%Pro%'
+
+--Task 23
