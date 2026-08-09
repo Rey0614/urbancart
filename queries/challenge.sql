a6a9893ab578565992471007ea31e6a3a3bc6f8b
Rey <95080700+Rey0614@users.noreply.github.com>
2026-08-10 00:12:13 +0700

    Medium Tier is Done

--- queries/challenge.sql
+++ queries/challenge.sql
@@ -46,3 +46,37 @@ GROUP BY category_id
 SELECT category_id, AVG(unit_price) AS average_price
 FROM Products
 GROUP BY category_id
+
+--Task 30
+SELECT category_id, COUNT(*) AS number_category
+FROM Products
+GROUP BY category_id
+HAVING COUNT(*) > 5
+
+--Task 31
+SELECT order_id, customer_name, order_date, [status]
+FROM Orders
+INNER JOIN Customers
+ON Orders.customer_id = Customers.customer_id
+
+--Task 32
+SELECT OrderDetails.order_id, Products.product_name, OrderDetails.quantity
+FROM OrderDetails
+INNER JOIN Products
+ON OrderDetails.product_id = Products.product_id
+
+--Task 33
+SELECT order_id, SUM(quantity*unit_price) AS total_revenue
+FROM OrderDetails
+GROUP BY order_id
+
+--Task 34
+SELECT employee_id, COUNT(*) AS total_order
+FROM Orders
+GROUP BY employee_id
+HAVING COUNT(*) > 3
+
+--Task 35
+SELECT *
+FROM Products
+WHERE supplier_id = 2 AND unit_price > 100
