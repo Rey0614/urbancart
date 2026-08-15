da0d4220da8f042adeb62f1edd785872a87e2830
Rey <95080700+Rey0614@users.noreply.github.com>
2026-08-15 11:00:10 +0700

    all tasks are done

--- queries/challenge.sql
+++ queries/challenge.sql
@@ -80,3 +80,81 @@ HAVING COUNT(*) > 3
 SELECT *
 FROM Products
 WHERE supplier_id = 2 AND unit_price > 100
+
+--Task 36
+SELECT *
+FROM Customers
+LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
+
+--Task 37
+SELECT *
+FROM Customers
+LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
+WHERE order_id IS NULL
+
+--Task 38
+SELECT *
+FROM OrderDetails
+RIGHT JOIN Products ON OrderDetails.product_id = Products.product_id
+
+--Task 39
+SELECT *
+FROM OrderDetails
+RIGHT JOIN Products ON OrderDetails.product_id = Products.product_id
+WHERE OrderDetails.order_detail_id IS NULL
+
+--Task 40
+SELECT *
+FROM Customers
+FULL JOIN Orders ON Customers.customer_id = Orders.customer_id
+
+--Task 41
+SELECT *
+FROM Categories
+CROSS JOIN Suppliers
+
+--Task 42
+SELECT *
+FROM Orders
+
+SELECT *
+FROM Customers
+
+SELECT *
+FROM OrderDetails
+
+SELECT *
+FROM Products
+
+SELECT customer_name, product_name, quantity, order_date
+FROM Orders
+INNER JOIN Customers ON Orders.customer_id = Customers.customer_id
+INNER JOIN OrderDetails ON Orders.order_id = OrderDetails.order_id
+INNER JOIN Products ON OrderDetails.product_id = Products.product_id
+
+--Task 43
+SELECT *
+FROM Orders
+
+SELECT *
+FROM OrderDetails
+
+SELECT *
+FROM Products
+
+SELECT *
+FROM Customers
+
+SELECT *
+FROM Employees
+
+SELECT TOP 1 Employees.employee_id, SUM(OrderDetails.unit_price*quantity) AS revenue
+FROM Orders
+INNER JOIN OrderDetails ON Orders.order_id = OrderDetails.order_id
+INNER JOIN Products ON OrderDetails.product_id = Products.product_id
+INNER JOIN Customers ON Orders.customer_id = Customers.customer_id
+INNER JOIN Employees ON Orders.employee_id = Employees.employee_id
+GROUP BY Employees.employee_id
+HAVING COUNT(*) > 2
+ORDER BY revenue DESC
+
